e8ddf826f0649f6e18ef7ad7f8ee635562c13357
unknown <andrevbigaran12@gmail.com>
2022-03-05 13:54:22 -0300

    fix

--- js/main.js
+++ js/main.js
@@ -72,10 +72,10 @@ jQuery(function($) {'use strict',
 $(function(){
 	$('section#content article div.btn').click(function(){
 		$(this).siblings('p.box').slideToggle();	
-		if($(this).text() == "Know less"){
+		if($(this).text() == "Learn more"){
 			$(this).text("Learn more");
 		}else{
-			$(this).text("Learn more");
+			$(this).text("Show less");
 		}
 	});
 });
